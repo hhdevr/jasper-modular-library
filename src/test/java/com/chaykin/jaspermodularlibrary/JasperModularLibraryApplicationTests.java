1db17c48900e2e5bf87b445b60943c66eb7b83e8
Dmitry Chaykin <hhdevr@gmail.com>
2026-01-19 06:07:55 +0600

    init commit

---
+++ src/test/java/com/chaykin/jaspermodularlibrary/JasperModularLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaykin.jaspermodularlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperModularLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
